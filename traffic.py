c85f1cc49aa3dc5aaa92a9908fba4779780d9e23
Ana Mentus <anmentus@gmail.com>
2022-11-15 10:25:30 -0500

    Update traffic.py

--- traffic.py
+++ traffic.py
@@ -1,6 +1,12 @@
 import pyshark
 
 cap = pyshark.LiveCapture(interface='\\Device\\NPF_{device}')
+headers = ['Protocol type', 'Source address', 'Source port', 'Destination address', 'Destination port', 'Time']
+
+with open("data.csv", 'w') as file:
+    dw = csv.DictWriter(file, delimiter=',',
+                        fieldnames=headers)
+    dw.writeheader()
 
 for packet in cap.sniff_continuously(packet_count=1):
      print('Protocol type:', packet.transport_layer, 'Source port:', packet[packet.transport_layer].srcport)
