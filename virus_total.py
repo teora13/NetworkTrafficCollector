a250e77a15ee5a4747fd7e9eea8eb6371b3f65ac
Ana Mentus <anmentus@gmail.com>
2022-12-03 09:39:03 -0500

    Update virus_total.py

--- virus_total.py
+++ virus_total.py
@@ -1,5 +1,12 @@
 import requests
 import json
+import csv
+from csv import writer
 
 df = pd.read_csv("data.csv")
 dropped = df[['Source address', 'Destination address']].unstack().reset_index(drop=True)
+
+headers = ['IP', 'Total vendors', 'Country', 'Protocol type', 'Source port', 'Destination port', 'Time']
+with open("result.csv", 'w') as file:
+    dw = csv.DictWriter(file, delimiter=',', fieldnames=headers)
+    dw.writeheader()
